abdc0e3b11187090c5199d53bb8804fa3bd3528f
SariMelina <mmkings_beba@hotmail.com>
2021-01-26 17:54:54 -0600

    Adding get and post services

--- services/students.service.js
+++ services/students.service.js
@@ -1,10 +1,16 @@
 const exec = require('../repository/execute.js')
 
 async function getStudents() {
-    const data = exec.execute(`SELECT * FROM students`)
+    const data = await exec.execute(`SELECT * FROM students`)
+    return data
+}
+
+async function postStudent(body) {
+    const data = await exec.execute(`INSERT INTO students VALUES (default, '${body.name}','${body.gender}','${body.phone}','${body.address}', ${body.age})`)    
     return data
 }
 
 module.exports = {
-    getStudents
+    getStudents,
+    postStudent
 }
