d8bb58ed5515a658efce3b8e61c0a9c059dd8ddd
SariMelina <mmkings_beba@hotmail.com>
2021-01-26 15:36:08 -0600

    Adding body-parser

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const app = express()
 require('dotenv').config()
 const students = require('./routes/students.route.js')
 const conec = require('./repository/connection/connection.js')
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 app.use('/v1/students', students)
 
 conec.connetion().then(msg => {
     app.listen(process.env.PORT, ()=> {
+        console.log("Success connection");
         console.log(`Server start in port: ${process.env.PORT}`);
     })
 }).catch(err => {
