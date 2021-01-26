2d07b87f351d61dc97a6ac55e2a0cd4659e81971
SariMelina <mmkings_beba@hotmail.com>
2021-01-26 17:57:02 -0600

    Adding the post controller and the validations

--- controller/student.controller.js
+++ controller/student.controller.js
@@ -5,6 +5,60 @@ async function getStudents(){
     return data
 }
 
+async function postStundent(body){
+    let isOk= false
+    let ok =  validate(body)
+    if(ok){
+        isOk = true
+        await services.postStudent(body)
+    }
+    return isOk
+}
+
+function validate(body){
+    let ok = false
+    if(body && typeof body === 'object' && Object.keys(body).length > 0){
+        if(body.hasOwnProperty('name')&&body.hasOwnProperty('phone')&&body.hasOwnProperty('gender')){            
+            if(validateName(body.name) && validatePhone(body.phone) && validateGender(data.gender)){
+                ok = true
+                console.log("Successful registration");
+            }
+        }
+    }
+    return ok
+}
+
+function validateName (name) {
+    let ok = false
+    if(name !== '' && name.length > 0){
+        ok = true
+    }else{
+        console.log("Error with the name");
+    }
+    return ok
+}
+
+function validatePhone (data) {
+    let ok = false
+    if(data !== '' && data.length === 10){
+        ok = true
+    }else{
+        console.log("Error with the phone");
+    }
+    return ok
+}
+
+function validateGender (data) {
+    let ok = false
+    if(data !== '' && data.length === 1 && (data === 'F' || data === 'M')){
+        ok = true
+    }else{
+        console.log("Error with the gender");
+    }
+    return ok
+}
+
 module.exports = {
-    getStudents
+    getStudents,
+    postStundent
 }
