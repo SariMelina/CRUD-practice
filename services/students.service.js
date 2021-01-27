3805bf1097bb800e2479c8bd22b6aa20d69fbdff
SariMelina <mmkings_beba@hotmail.com>
2021-01-27 01:00:12 -0600

    Optimizing and exporting new service

--- services/students.service.js
+++ services/students.service.js
@@ -1,34 +1,22 @@
 const exec = require('../repository/execute.js')
 
-async function getStudents() {
-    const data = await exec.execute(`SELECT * FROM students`)
-    return data
-}
+const getStudents = () => exec.execute(`SELECT * FROM students`)
 
-async function getStudent(id){
-    const data = await exec.execute(`SELECT * FROM students WHERE id = ${id}`)
-    return data
-}
+const getStudent = (id) => exec.execute(`SELECT * FROM students WHERE id = ${id}`)
 
-async function postStudent(body) {
-    const data = await exec.execute(`INSERT INTO students VALUES (default, '${body.name}','${body.gender}','${body.phone}','${body.address}', ${body.age})`)    
-    return data
-}
+const postStudent = (body) => exec.execute(`INSERT INTO students VALUES (default, '${body.name}','${body.gender}','${body.phone}','${body.address}', ${body.age})`)    
 
-async function updateStudent(id, body){
-    const data = await exec.execute(`UPDATE students SET name='${body.name}',gender='${body.gender}',phone='${body.phone}',address='${body.address}',age= ${body.age} WHERE id=${id}`)
-    return data
-}
+const updateStudent = (id, body) => exec.execute(`UPDATE students SET name='${body.name}',gender='${body.gender}',phone='${body.phone}',address='${body.address}',age= ${body.age} WHERE id=${id}`)
 
-async function deleteStudent(id){
-    const data = await exec.execute(`DELETE FROM students WHERE id=${id}`)
-    return data
-}
+const deleteStudent = (id) => exec.execute(`DELETE FROM students WHERE id=${id}`)
+
+const getId = (id) => exec.execute(`SELECT * FROM students WHERE id=${id}`)
 
 module.exports = {
     getStudents,
     getStudent,
     postStudent,
     updateStudent,
-    deleteStudent
+    deleteStudent,
+    getId
 }
