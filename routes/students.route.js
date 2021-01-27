834cd807c163535c55365a712f953c8acf34d439
SariMelina <mmkings_beba@hotmail.com>
2021-01-27 01:01:32 -0600

    Adding the validate into the routes

--- routes/students.route.js
+++ routes/students.route.js
@@ -1,22 +1,23 @@
 const express = require('express')
 const student = express.Router()
 const ctrStundents = require('../controller/student.controller.js')
+const validate = require('../middleware/idValidation.middle.js')
 
 student.get('/',async(req, res)=> {
     const data = await ctrStundents.getStudents()
     return res.status(200).send({
         status:200,
         msg: "Success",
-        data: data
+        data: data[0]  
     })
 });
 
-student.get('/:id', async(req,res)=> {
+student.get('/:id',validate, async(req,res)=> {
     const data = await ctrStundents.getStudent(req.params.id)
     return res.status(200).send({
         status:200,
         msg:"Success",
-        data: data    
+        data: data[0]
     })
     
 })
@@ -36,7 +37,7 @@ student.post('/', async (req, res) => {
     })
 })
 
-student.put('/:id', async (req, res)=>{
+student.put('/:id',validate, async (req, res)=>{
     let { body, params } = req
     let status = 201
     let ok = await ctrStundents.updateStudent(params.id, body)
@@ -51,14 +52,15 @@ student.put('/:id', async (req, res)=>{
     })
 })
 
-student.delete('/:id', async(req, res)=> {
+student.delete('/:id',validate, async(req, res)=> {
     let { params } = req
     let status = 200
     let ok = await ctrStundents.deleteStudent(params.id)
+    console.log(ok);
     let msg = 'Student deleted '
     if(ok === false){
         status = 400
-        msg = 'The id doesnt exist'
+        msg = 'Error'
     }
     return res.status(status).send({
         status:status,
